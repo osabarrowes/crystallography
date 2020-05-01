bf3ea20f5e8b5b34ebe25300be1d717c29c1cbe0
xenonni <osabarrowes@yahoo.com>
2020-04-30 19:26:42 -0600

    Organizational setup. Added LibReferences.java to contain constants referenced in other parts of the mod.

---
+++ src/main/java/mvp/common/lib/LibReferences.java
@@ -0,0 +1,12 @@
+/**
+ * Content here is referenced in other parts of the mod.
+ */
+
+package mvp.common.lib;
+
+public final class LibReferences {
+
+    // Mod Constants
+    public static final String MOD_ID = "mvp";
+    public static final String MOD_NAME= "Minimum Viable Product";
+}
