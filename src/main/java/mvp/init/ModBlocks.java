a544f1df4e08d6f49f98c9389eb6ab3ba6a1596c
xenonni <osabarrowes@yahoo.com>
2020-05-03 19:48:03 -0600

    Removing blocks from the mod's registration does NOT remove them from the save data of worlds created (how else would worlds know if they contain unregistered blocks?) If you remove a block from the mod's registration, you need to recreate the world you're testing with unless you like unregistered blocks floating around in the aether (may be invisible, as it seems forge sets unregistered blocks to air)

--- src/main/java/mvp/init/ModBlocks.java
+++ src/main/java/mvp/init/ModBlocks.java
@@ -26,5 +26,5 @@ public class ModBlocks {
     // This block has the ROCK material, meaning it needs at least a wooden pickaxe to break it. It is very similar to Iron Ore
     public static final RegistryObject<Block> EXAMPLE_ORE = BLOCKS.register("example_ore", () -> new Block(Block.Properties.create(Material.ROCK).hardnessAndResistance(3.0F, 3.0F)));
     public static final RegistryObject<Block> EXAMPLE_ORE_2 = BLOCKS.register("example_ore_2", () -> new Block(Block.Properties.create(Material.ROCK).hardnessAndResistance(3.0F, 3.0F)));
-
+    public static final RegistryObject<Block> TEST_BLOCK = BLOCKS.register("test_block", () -> new Block(Block.Properties.create(Material.ROCK).hardnessAndResistance(3.0F, 3.0F)));
 }
