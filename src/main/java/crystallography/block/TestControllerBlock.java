8ad4f08641de7e36723c87ae1ae6ce317287feac
xenonni <osabarrowes@yahoo.com>
2020-06-04 09:48:37 -0600

    Added logic to ControllerBlock for determining properties of the overall structure. Added TestControllerBlock to exist as a non-abstract implementation of ControllerBlock (note that it has not been registered yet)

---
+++ src/main/java/crystallography/block/TestControllerBlock.java
@@ -0,0 +1,51 @@
+package crystallography.block;
+
+import crystallography.libs.multiblock.ControllerBlock;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.item.BlockItemUseContext;
+import net.minecraft.state.BooleanProperty;
+import net.minecraft.state.StateContainer;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+/**
+ * Used to make experimental changes to blocks. Mostly for figuring out how to do things.
+ */
+public class TestControllerBlock extends ControllerBlock {
+
+    public TestControllerBlock(Properties properties) { super(properties); }
+
+    public static final BooleanProperty VALID = BooleanProperty.create("valid"); // This has the same name as a property in TestBlock. Hopefully that doesn't cause problems
+
+    // All IProperties used in a BlockState are added here.
+    @Override
+    protected void fillStateContainer(final StateContainer.Builder<Block, BlockState> builder) {
+        super.fillStateContainer(builder);
+        builder.add(VALID);
+    }
+
+    // The default state of all IProperties is set here.
+    @Override
+    public BlockState getStateForPlacement(BlockItemUseContext context) {
+        return this.getDefaultState().with(VALID, false);
+    }
+
+    @Override
+    public boolean isValid(World worldIn, BlockPos pos)
+    {
+        CuboidCategory result = CuboidCategory.categorize(worldIn, pos);
+        final BlockState newState;
+        if (result.equals(CuboidCategory.ILLEGAL)) {
+            newState = worldIn.getBlockState(pos).with(VALID, false);
+        }
+        else
+        {
+            newState = worldIn.getBlockState(pos).with(VALID, true);
+        }
+        // Flag 2: send the change to clients
+        worldIn.setBlockState(pos, newState, 2);
+
+        return newState.get(VALID) && super.isValid(worldIn, pos);
+    }
+}
--- src/main/java/crystallography/libs/multiblock/ControllerBlock.java
+++ src/main/java/crystallography/libs/multiblock/ControllerBlock.java
@@ -9,7 +9,7 @@ import java.util.Set;
  * Controller blocks exist as components of a multiblock and serve as the overseer or master. Logic which applies to the
  * entire multiblock structure should be handled by the ControllerBlock.
  *
- * Exactly one controllerBlock is allowed per multiblock structure.
+ * Exactly one controllerBlock is required per multiblock structure.
  */
 public abstract class ControllerBlock extends MultiBlockComponent {
 
@@ -22,22 +22,28 @@ public abstract class ControllerBlock extends MultiBlockComponent {
         super(properties);
     }
 
-    /*
-     * Exactly one controllerBlock is allowed per multiblock structure.
-     *
-      */
     @Override
     public boolean isValid(World worldIn, BlockPos pos)
     {
+        int controllerCount = 0;
+        int cornerCount = 0;
         for(BlockPos comp : structure)
         {
             if(worldIn.getBlockState(comp).getBlock() instanceof ControllerBlock)
-                return false;
+            {
+                controllerCount++;
+            }
+
+            if(CuboidCategory.categorize(worldIn, comp).equals(CuboidCategory.CORNER))
+            {
+                cornerCount++;
+            }
         }
-        structure.add(pos);
-        // return imValid(worldIn, pos, structure); // FIXME
+
+        if(controllerCount != 1 || cornerCount != 8) {
+            return false;
+        }
+
         return true;
     }
-    // ControllerBlock will start the algorithm initially, but any MultiBlockComponent should be able to do so.
-
 }
